59b74950e7e167ea729186db29e8db904e0030c0
Ganesh-1907 <ganesh.bora@artofliving.org>
2025-12-07 12:00:07 +0530

    [bundle id] change bundle id to com.coliv.managers

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.coliv.manager',
+  appId: 'com.coliv.managers',
   appName: 'CoHub - Property Management',
   webDir: 'dist'
 };
